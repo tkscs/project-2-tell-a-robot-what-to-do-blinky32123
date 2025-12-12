4b142507b0a8a34aa9d6196e1c7f7bdb9955d3dc
blinky32123 <jkrepner2028@kehillahstudent.org>
2025-12-11 22:53:49 -0800

    ee

--- main.py
+++ main.py
@@ -5,4 +5,144 @@ from simulator import robot, FORWARD, BACKWARD, STOP
 # Use robot.left_sonar() and robot.right_sonar() to sense obstacles
 
 # When you're done, close the simulator
+
+ 
+#run terminal to find error code for pygame and numpy for simulator and then copy and paste error code in google so then you can fix it(depends on what kind of computer)
+def idle():
+    robot.motors(left=FORWARD, right=FORWARD, seconds=0)
+
+
+def wall_check( left, right ):
+    if left <25 and right <25:
+        print ( "too close, go backward and stop")
+        robot.motors(left=STOP, right=STOP, seconds=0.1)
+        robot.motors(left=BACKWARD, right=BACKWARD, seconds=1.5)
+        robot.motors(left=BACKWARD, right=FORWARD, seconds=1.4) #rotate 180
+        return "stop" 
+    elif left <25:
+        print ( "turn right")
+        robot.motors(left=STOP, right=STOP, seconds=0.1)
+        robot.motors(left=BACKWARD, right=BACKWARD, seconds=0.5)
+        robot.motors(left=BACKWARD, right=FORWARD, seconds=1.4) #rotate away from wall(going left)
+    elif right<25:
+        print ( "turn left")
+        robot.motors(left=STOP, right=STOP, seconds=0.1)
+        robot.motors(left=BACKWARD, right=BACKWARD, seconds=0.5)
+        robot.motors(left=FORWARD, right=BACKWARD, seconds=1.4) #rotate away from wall(going right)
+    else:
+        print ( "continue")
+    
+    return "continue"
+
+def follow_wall():
+    order = "continue"
+
+    while order!="stop":
+        robot.motors(left=FORWARD,right=FORWARD, seconds=0.1)
+        right_distance=robot.right_sonar()
+        left_distance=robot.left_sonar()
+        print(f"left / right distance {left_distance} {right_distance} ")
+
+        order = wall_check( left_distance, right_distance )
+
+#        if left_distance <25 and right_distance<25:
+#            print ( "go back then turn 180")
+#            robot.motors(left=STOP, right=STOP, seconds=0.1)
+#            robot.motors(left=BACKWARD, right=BACKWARD, seconds=1)
+#            robot.motors(left=FORWARD, right=BACKWARD, seconds=1.2) 
+#        elif left_distance <25:
+#            print ( "turn right")
+#            robot.motors(left=STOP, right=STOP, seconds=0.1)
+#            robot.motors(left=BACKWARD, right=BACKWARD, seconds=0.5)
+#            robot.motors(left=BACKWARD, right=FORWARD, seconds=1.4) #rotate away from wall(going left)
+#        elif right_distance<25:
+#            print ( "turn left")
+#            robot.motors(left=STOP, right=STOP, seconds=0.1)
+#            robot.motors(left=BACKWARD, right=BACKWARD, seconds=0.5)
+#            robot.motors(left=FORWARD, right=BACKWARD, seconds=1.4) #rotate away from wall(going right)
+#        else:
+#            print ( "continue")
+
+
+
+
+
+
+def left_circle_draw():
+    for i in range(360):
+        robot.motors(left=FORWARD,right=FORWARD, seconds=0.01)
+        robot.motors(left=BACKWARD, right=FORWARD, seconds=0.01)
+def right_circle_draw():
+    for i in range(360):
+        robot.motors(left=FORWARD,right=FORWARD, seconds=0.01)
+        robot.motors(left=FORWARD, right=BACKWARD, seconds=0.01)
+
+#tester
+# for i in range(100):
+#      robot.motors(right=FORWARD, left=FORWARD, seconds=1)
+#      wall_check()
+
+def both_forward(): 
+    robot.motors(left=FORWARD,right=FORWARD, seconds=2)
+    wall_check( robot.left_sonar(), robot.right_sonar() )
+
+
+def star(): #72 degrees for star #wip
+    for i in range(5):
+        both_forward()
+        robot.motors(left=BACKWARD, right=FORWARD, seconds=0.95)
+
+
+def figure_eight():
+    left_circle_draw()
+    right_circle_draw()
+
+
+
+def right_donut():
+    robot.motors(left=FORWARD, seconds=5)
+    wall_check( robot.left_sonar(), robot.right_sonar() )
+
+def left_donut():
+    robot.motors(right=FORWARD, seconds=5)
+    wall_check( robot.left_sonar(), robot.right_sonar() )
+
+def right_spin():
+    robot.motors(right=FORWARD, left=-BACKWARD, seconds=1.525)
+    wall_check( robot.left_sonar(), robot.right_sonar() )
+    
+def left_spin():
+    robot.motors(right=-FORWARD, left=BACKWARD, seconds=1.525)
+    wall_check( robot.left_sonar(), robot.right_sonar() )
+
+
+
+def placeholder():
+    question=input("what movement would you like the robot to do./n")
+    if question == "swerve":
+        swerve()
+        print("swerving")
+
+robot_state="idle"
+
+while robot_state!="off":
+    robot_state = input("What would you like the robot to do? (check wall,idle,follow wall,off,8)")
+    if robot_state=="check wall":
+        print("The state is check wall")
+        wall_check( robot.left_sonar(), robot.right_sonar() )
+    elif robot_state=="idle":
+        print("The state is idle")
+        idle()
+    elif robot_state=="follow wall": #follow wall boundaries #wip
+        print("The state is follow wall")
+        follow_wall()
+    elif robot_state=="off":
+        print("robo terminated")
+        break
+    elif robot_state=="8":
+        print("The state is eight")
+        figure_eight()
+    else:
+        print(f"Error! The state {robot_state} does not exist")
+
 robot.exit()
--- robot.py
+++ robot.py
@@ -1,113 +1,17 @@
-import RPi.GPIO as GPIO
-import time
+from simulator import robot, FORWARD, BACKWARD, STOP
 
-# Real Robot Driver
-class RealRobotDriver:
-    def __init__(self):
-        print("robot driver initializing...") 
-        #GPIO Mode (BOARD / BCM)
-        GPIO.setmode(GPIO.BCM)
+# TODO: Write your code here!
+# Use robot.motors() to move
+# Use robot.left_sonar() and robot.right_sonar() to sense obstacles
 
-        #set GPIO Pins for Sonar Sensors
-        self.GPIO_LEFT_TRIGGER = 5
-        self.GPIO_LEFT_ECHO = 6
-        self.GPIO_RIGHT_TRIGGER = 17
-        self.GPIO_RIGHT_ECHO = 27
+# When you're done, close the simulator
 
-        # set GPIO pins for motors
-        self.GPIO_LEFT_MOTOR_SPEED = 12
-        self.GPIO_LEFT_MOTOR_BLUE = 1
-        self.GPIO_LEFT_MOTOR_ORANGE = 7
-        self.GPIO_RIGHT_MOTOR_SPEED = 18
-        self.GPIO_RIGHT_MOTOR_BLUE = 24
-        self.GPIO_RIGHT_MOTOR_ORANGE = 23
+ 
+#run terminal to find error code for pygame and numpy for simulator and then copy and paste error code in google so then you can fix it(depends on what kind of computer)
+def idle():# replace with break function so a loop isnt needeD?
+    robot.motors(left=FORWARD, right=FORWARD, seconds=0)
 
-        #set GPIO direction (IN / OUT)
-        GPIO.setup(self.GPIO_LEFT_TRIGGER, GPIO.OUT)
-        GPIO.setup(self.GPIO_LEFT_ECHO, GPIO.IN)
-        GPIO.setup(self.GPIO_RIGHT_TRIGGER, GPIO.OUT)
-        GPIO.setup(self.GPIO_RIGHT_ECHO, GPIO.IN)
-
-        GPIO.setup(self.GPIO_LEFT_MOTOR_SPEED, GPIO.OUT)
-        GPIO.setup(self.GPIO_LEFT_MOTOR_BLUE, GPIO.OUT)
-        GPIO.setup(self.GPIO_LEFT_MOTOR_ORANGE, GPIO.OUT)
-        GPIO.setup(self.GPIO_RIGHT_MOTOR_SPEED, GPIO.OUT)
-        GPIO.setup(self.GPIO_RIGHT_MOTOR_BLUE, GPIO.OUT)
-        GPIO.setup(self.GPIO_RIGHT_MOTOR_ORANGE, GPIO.OUT)
-
-    def left_sonar(self):
-        return self.sonar(self.GPIO_LEFT_TRIGGER, self.GPIO_LEFT_ECHO)
     
-    def right_sonar(self):
-        return self.sonar(self.GPIO_RIGHT_TRIGGER, self.GPIO_RIGHT_ECHO)
 
-    def sonars(self):
-        left_distance = self.sonar(self.GPIO_LEFT_TRIGGER, self.GPIO_LEFT_ECHO)
-        right_distance = self.sonar(self.GPIO_RIGHT_TRIGGER, self.GPIO_RIGHT_ECHO)
-        return left_distance, right_distance
 
-    def sonar(self, GPIO_TRIGGER, GPIO_ECHO):
-        # set Trigger to HIGH
-        GPIO.output(GPIO_TRIGGER, True)
-
-        # set Trigger after 0.01ms to LOW
-        time.sleep(0.00001)
-        GPIO.output(GPIO_TRIGGER, False)
-
-        StartTime = time.time()
-        StopTime = time.time()
-
-        # save StartTime
-        while GPIO.input(GPIO_ECHO) == 0:
-            StartTime = time.time()
-            #print(f"{StartTime=}")
-
-        # save time of arrival
-        while GPIO.input(GPIO_ECHO) == 1:
-            StopTime = time.time()
-            #print(f"{StopTime=}")
-
-        # time difference between start and arrival
-        TimeElapsed = StopTime - StartTime
-        # multiply with the sonic speed (34300 cm/s)
-        # and divide by 2, because there and back
-        distance = (TimeElapsed * 34300) / 2
-
-        return distance
-    
-    def stop(self):
-        GPIO.output(self.GPIO_LEFT_MOTOR_BLUE, GPIO.LOW)
-        GPIO.output(self.GPIO_LEFT_MOTOR_ORANGE, GPIO.LOW)
-        GPIO.output(self.GPIO_LEFT_MOTOR_SPEED, GPIO.LOW)
-        GPIO.output(self.GPIO_RIGHT_MOTOR_BLUE, GPIO.LOW)
-        GPIO.output(self.GPIO_RIGHT_MOTOR_ORANGE, GPIO.LOW)
-        GPIO.output(self.GPIO_RIGHT_MOTOR_SPEED, GPIO.LOW)
-        GPIO.output(self.GPIO_LEFT_TRIGGER, GPIO.LOW)
-        GPIO.output(self.GPIO_RIGHT_TRIGGER, GPIO.LOW)
-
-    def motor(self, velocity, speed_pin, blue_pin, orange_pin):
-        if velocity == 0:
-            GPIO.output(blue_pin, GPIO.LOW)
-            GPIO.output(orange_pin, GPIO.LOW)
-            GPIO.output(speed_pin, GPIO.LOW)
-        elif velocity > 0:
-            GPIO.output(blue_pin, GPIO.HIGH)
-            GPIO.output(orange_pin, GPIO.LOW)
-            GPIO.output(speed_pin, GPIO.HIGH)
-        else:
-            GPIO.output(blue_pin, GPIO.LOW)
-            GPIO.output(orange_pin, GPIO.HIGH)
-            GPIO.output(speed_pin, GPIO.HIGH)
-    
-    def motors(self, left, right, seconds):
-        # Call real robot hardware control for left motor
-        #self.robot_hardware.set_left_motor_speed(left)
-        #self.robot_hardware.set_right_motor_speed(right)
-        self.motor(left, self.GPIO_LEFT_MOTOR_SPEED, self.GPIO_LEFT_MOTOR_BLUE, self.GPIO_LEFT_MOTOR_ORANGE)
-        self.motor(right, self.GPIO_RIGHT_MOTOR_SPEED, self.GPIO_RIGHT_MOTOR_BLUE, self.GPIO_RIGHT_MOTOR_ORANGE)
-        time.sleep(seconds)
-        self.stop()
-
-    def exit(self):
-        self.stop()
-        return
+robot.exit()
